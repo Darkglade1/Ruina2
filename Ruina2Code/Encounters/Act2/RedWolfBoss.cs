18df210b93b8ecb62bbe5c144d6dc67bce4421fc
Darkglade1 <30379754+Darkglade1@users.noreply.github.com>
2026-06-30 21:58:35 -0400

    add fight background

--- Ruina2Code/Encounters/Act2/RedWolfBoss.cs
+++ Ruina2Code/Encounters/Act2/RedWolfBoss.cs
@@ -1,7 +1,9 @@
 using BaseLib.Abstracts;
+using BaseLib.Utils;
 using Godot;
 using MegaCrit.Sts2.Core.Entities.Encounters;
 using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.Random;
 using MegaCrit.Sts2.Core.Rooms;
 using Ruina2.Ruina2Code.Extensions;
 using Ruina2.Ruina2Code.Monsters.Act2.redWolf;
@@ -16,6 +18,13 @@ public sealed class RedWolfBoss : CustomEncounterModel
     public override string BossNodePath => "LittleRed/Red".MonsterImagePath().SimplifyPath();
     public override string? CustomRunHistoryIconOutlinePath => "LittleRed/Red.png".MonsterImagePath().SimplifyPath();
     public override string? CustomRunHistoryIconPath => "LittleRed/RedOutline.png".MonsterImagePath().SimplifyPath();
+    public override CustomBackgroundAssets? CustomEncounterBackground(ActModel parentAct, Rng rng)
+    {
+        return new CustomBackgroundAssets("res://BaseLib/scenes/dynamic_background.tscn",
+            ["LittleRed/background/night_forest_bg_00.tscn".MonsterImagePath()], 
+            "LittleRed/background/night_forest_bg_00.tscn".MonsterImagePath());
+    }
+    public override float GetCameraScaling() => 0.9f;
     public override bool IsValidForAct(ActModel act) => act is Briah;
 
     public override IEnumerable<EncounterTag> Tags => Array.Empty<EncounterTag>();
