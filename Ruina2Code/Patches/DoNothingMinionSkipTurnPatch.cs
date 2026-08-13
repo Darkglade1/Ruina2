43fbf69b372523b7b8a34533fa693c304c5d09bc
Darkglade1 <30379754+Darkglade1@users.noreply.github.com>
2026-08-13 16:23:07 -0400

    hacky solution to make twilight turn resolution feel better

--- Ruina2Code/Patches/DoNothingMinionSkipTurnPatch.cs
+++ Ruina2Code/Patches/DoNothingMinionSkipTurnPatch.cs
@@ -1,15 +1,18 @@
 ﻿using HarmonyLib;
 using MegaCrit.Sts2.Core.Models;
+using Ruina2.Ruina2Code.Monsters.Act2.Greed;
+using Ruina2.Ruina2Code.Monsters.Act2.mountain;
+using Ruina2.Ruina2Code.Monsters.Act2.RoadHome;
 using Ruina2.Ruina2Code.Monsters.Act3.Twilight;
 
 namespace Ruina2.Ruina2Code.Patches;
 
 [HarmonyPatch(typeof(MonsterModel), nameof(MonsterModel.PerformMove))]
-public static class EggSkipTurnPatch
+public static class DoNothingMinionSkipTurnPatch
 {
     public static bool Prefix(MonsterModel __instance, ref Task __result)
     {
-        if (__instance is BigEgg || __instance is SmallEgg || __instance is LongEgg)
+        if (__instance is BigEgg || __instance is SmallEgg || __instance is LongEgg || __instance is Corpse || __instance is BrilliantBliss || __instance is Home)
         {
             __result = Task.CompletedTask;
             return false;
