c28edb715a185036b6f1715dc7fae31a854788ff
Darkglade1 <30379754+Darkglade1@users.noreply.github.com>
2026-08-27 15:45:04 -0400

    add ego cards to merchant

--- Ruina2Code/Cards/EGO/EGOCardPool.cs
+++ Ruina2Code/Cards/EGO/EGOCardPool.cs
@@ -1,6 +1,8 @@
 using BaseLib.Abstracts;
 using Godot;
 using MegaCrit.Sts2.Core.Assets;
+using MegaCrit.Sts2.Core.Models;
+using Ruina2.Ruina2Code.Cards.EGO.Act2;
 using Ruina2.Ruina2Code.Extensions;
 
 namespace Ruina2.Ruina2Code.Cards.EGO;
@@ -31,6 +33,16 @@ public class EGOCardPool : CustomCardPoolModel
     public override Color DeckEntryCardColor => Color.Color8(69, 2, 30);
     
     public override bool IsShared => true;
-
     public override bool IsColorless => true;
+
+    public static List<CardModel> GetAct2EgoCards()
+    {
+        List<CardModel> cards = new List<CardModel>();
+        cards.Add(ModelDb.Card<BlindRage>());
+        cards.Add(ModelDb.Card<CobaltScar>());
+        cards.Add(ModelDb.Card<CrimsonScar>());
+        cards.Add(ModelDb.Card<GoldRush>());
+        cards.Add(ModelDb.Card<LoveAndHate>());
+        return cards;
+    }
 }
---
+++ Ruina2Code/Patches/MerchantEgoCardPatch.cs
@@ -0,0 +1,41 @@
+﻿using Godot;
+using HarmonyLib;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.Entities.Merchant;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.Runs;
+using Ruina2.Ruina2Code.Acts;
+using Ruina2.Ruina2Code.Cards.EGO;
+
+namespace Ruina2.Ruina2Code.Patches;
+
+[HarmonyPatch(typeof(MerchantInventory), nameof(MerchantInventory.PopulateColorlessCardEntries))]
+public static class MerchantEgoCardPatch
+{
+    public static void Postfix(MerchantInventory __instance)
+    {
+        if (RunManager.Instance.State?.Act is AbstractRuinaAct)
+        {
+            __instance._colorlessCardEntries.Clear();
+            List<CardModel> list = EGOCardPool.GetAct2EgoCards();
+            for (int i = 0; i < 2; i++)
+            {
+                MerchantCardEntry merchantCardEntry = new MerchantCardEntry(__instance.Player, __instance, list, CardRarity.Rare);
+                merchantCardEntry.Populate();
+                __instance._colorlessCardEntries.Add(merchantCardEntry);
+            }
+        }
+    }
+}
+
+[HarmonyPatch(typeof(MerchantCardEntry), nameof(MerchantCardEntry.GetCost))]
+public static class MerchantEgoCardPricePatch
+{
+    public static void Postfix(CardModel card, ref int __result)
+    {
+        if (card.Pool is EGOCardPool)
+        { 
+            __result = Mathf.RoundToInt(__result * 1.15f);
+        }
+    }
+}
