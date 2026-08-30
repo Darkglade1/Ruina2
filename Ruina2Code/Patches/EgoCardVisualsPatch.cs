c46aedfb591fe8fbbc2e219e53f80677df9005a0
Darkglade1 <30379754+Darkglade1@users.noreply.github.com>
2026-08-29 21:36:15 -0400

    i don't know why I have to do this now but w/e I guess

--- Ruina2Code/Patches/EgoCardVisualsPatch.cs
+++ Ruina2Code/Patches/EgoCardVisualsPatch.cs
@@ -1,6 +1,5 @@
 ﻿using Godot;
 using HarmonyLib;
-using MegaCrit.Sts2.Core.Models;
 using MegaCrit.Sts2.Core.Nodes.Cards;
 using Ruina2.Ruina2Code.Cards.EGO;
 
@@ -16,8 +15,10 @@ public static class EgoCardVisualsPatch
             if (__instance.Model is EGOCard egoCard)
             {
                 __instance._typePlaque.Texture = ResourceLoader.Load<Texture2D>(egoCard.CustomPlaqueTexturePath);
-                __instance._portraitBorder.Material = null;
                 __instance._typePlaque.Material = null;
+                __instance._portraitBorder.Texture = ResourceLoader.Load<Texture2D>(egoCard.CustomPortraitBorderPath);
+                __instance._portraitBorder.Material = null;
+                __instance._banner.Texture = ResourceLoader.Load<Texture2D>(egoCard.CustomBannerTexturePath);
                 __instance._banner.Material = null;
             }
             else
@@ -26,29 +27,5 @@ public static class EgoCardVisualsPatch
                     ResourceLoader.Load<Texture2D>("res://images/ui/cards/card_portrait_border_plaque2.png");
             }
         }
-    }
-}
-
-[HarmonyPatch(typeof (CardModel), "BannerTexture", MethodType.Getter)]
-public static class EgoBannerTexturePatch
-{
-    private static bool Prefix(CardModel __instance, ref Texture2D? __result)
-    {
-        if (!(__instance is EGOCard egoCard))
-            return true;
-        __result = ResourceLoader.Load<Texture2D>(egoCard.CustomBannerTexturePath);
-        return __result == null;
-    }
-}
-
-[HarmonyPatch(typeof (CardModel), "PortraitBorder", MethodType.Getter)]
-public static class EgoCardPortraitBorderPatch
-{
-    private static bool Prefix(CardModel __instance, ref Texture2D? __result)
-    {
-        if (!(__instance is EGOCard egoCard))
-            return true;
-        __result = ResourceLoader.Load<Texture2D>(egoCard.CustomPortraitBorderPath);
-        return __result == null;
     }
 }
