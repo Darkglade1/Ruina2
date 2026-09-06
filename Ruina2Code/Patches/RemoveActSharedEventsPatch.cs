089a8eb103ca3a278b6ca4fd4a737589a441664c
Darkglade1 <30379754+Darkglade1@users.noreply.github.com>
2026-09-06 11:32:45 -0400

    remove hack

--- Ruina2Code/Patches/RemoveActSharedEventsPatch.cs
+++ Ruina2Code/Patches/RemoveActSharedEventsPatch.cs
@@ -2,10 +2,8 @@
 using MegaCrit.Sts2.Core.Extensions;
 using MegaCrit.Sts2.Core.Models;
 using MegaCrit.Sts2.Core.Random;
-using MegaCrit.Sts2.Core.Runs;
 using MegaCrit.Sts2.Core.Unlocks;
 using Ruina2.Ruina2Code.Acts;
-using Ruina2.Ruina2Code.Events;
 
 namespace Ruina2.Ruina2Code.Patches;
 
@@ -19,16 +17,6 @@ public static class RemoveActSharedEventsPatch
             __instance._rooms.events.Clear();
             var list = __instance.AllEvents.ToList();
             __instance._rooms.events.AddRange(list.UnstableShuffle(rng));
-        }
-
-        // Hack to stop ancient config from overriding Neow Angela
-        // This is probably load order dependent but Ritsulib sort seems to put the mods in the right order
-        if (__instance is Asiyah)
-        {
-            if (RunManager.Instance.State != null && RunManager.Instance.State.Modifiers.Count == 0 && Config.NeowAngelaAppears)
-            {
-                __instance._rooms._ancient = ModelDb.AncientEvent<NeowAngela>();
-            }
         }
     }
 }
