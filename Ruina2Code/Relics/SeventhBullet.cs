34a94cde14ed6521add0caaa4e02ecb87fb6ac63
Darkglade1 <30379754+Darkglade1@users.noreply.github.com>
2026-09-07 16:09:44 -0400

    add 2 more act 1 events and fix seventh bullet to be save safe

--- Ruina2Code/Relics/SeventhBullet.cs
+++ Ruina2Code/Relics/SeventhBullet.cs
@@ -4,16 +4,18 @@ using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.Entities.Relics;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Helpers;
 using MegaCrit.Sts2.Core.HoverTips;
 using MegaCrit.Sts2.Core.Localization.DynamicVars;
 using MegaCrit.Sts2.Core.Models.Powers;
 using MegaCrit.Sts2.Core.Models.RelicPools;
+using MegaCrit.Sts2.Core.Saves.Runs;
 using MegaCrit.Sts2.Core.ValueProps;
 
 namespace Ruina2.Ruina2Code.Relics;
 
 [Pool(typeof(EventRelicPool))]
-public class SeventhBullet() : Ruina2Relic
+public class SeventhBullet : Ruina2Relic
 {
     public override RelicRarity Rarity =>
         RelicRarity.Event;
@@ -23,11 +25,44 @@ public class SeventhBullet() : Ruina2Relic
 
     protected override IEnumerable<IHoverTip> ExtraHoverTips => [HoverTipFactory.FromPower<StrengthPower>()];
 
+    private bool _isActivating;
     private int _attacksPlayed;
+    
+    public bool IsActivating
+    {
+        get => _isActivating;
+        set
+        {
+            AssertMutable();
+            _isActivating = value;
+            UpdateDisplay();
+        }
+    }
+    
+    [SavedProperty]
+    public int AttacksPlayed
+    {
+        get => _attacksPlayed;
+        set
+        {
+            AssertMutable();
+            _attacksPlayed = value;
+            UpdateDisplay();
+        }
+    }
+    
+    public void UpdateDisplay()
+    {
+        if (IsActivating)
+            Status = RelicStatus.Normal;
+        else
+            Status = AttacksPlayed == DynamicVars.Cards.IntValue - 1 ? RelicStatus.Active : RelicStatus.Normal;
+        InvokeDisplayAmountChanged();
+    }
     
     public override bool ShowCounter => true;
     
-    public override int DisplayAmount => _attacksPlayed;
+    public override int DisplayAmount => !IsActivating ? AttacksPlayed % DynamicVars.Cards.IntValue : DynamicVars.Cards.IntValue;
 
     public override async Task BeforeCombatStart()
     {
@@ -39,14 +74,22 @@ public class SeventhBullet() : Ruina2Relic
     {
         if (cardPlay.Card.Owner == Owner && CombatManager.Instance.IsInProgress && cardPlay.Card.Type == CardType.Attack)
         {
-            _attacksPlayed++;
-            if (_attacksPlayed >= DynamicVars.Cards.IntValue)
+            AttacksPlayed++;
+            if (AttacksPlayed >= DynamicVars.Cards.IntValue)
             {
-                Flash();
-                _attacksPlayed = 0;
+                TaskHelper.RunSafely(DoActivateVisuals());
                 await CreatureCmd.Damage(choiceContext, Owner.Creature, DynamicVars.Damage, Owner.Creature);
+                AttacksPlayed = 0;
             }
             InvokeDisplayAmountChanged();
         }
+    }
+    
+    public async Task DoActivateVisuals()
+    {
+        IsActivating = true;
+        Flash();
+        await Cmd.Wait(1f);
+        IsActivating = false;
     }
 }
